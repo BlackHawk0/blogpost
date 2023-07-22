5b0d96395976e8f93f33956a4ffb67e0b2518845
BlackHawk0 <danielngoys@outlook.com>
2023-07-22 04:59:34 +0300

    ✨: render followers

---
+++ src/Pages/Followers.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/context';
+
+const FollowingPosts = () => {
+  const { users, following, posts } = useContext(AppContext);
+
+  // Get the posts of the users the authenticated user is following
+  const followingPosts = posts.filter((post) => following.includes(post.userId));
+
+  return (
+    <div>
+      <h1>Following</h1>
+      {followingPosts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FollowingPosts;
