2a6d5d595d525542fdd63ff94856cd3d875616c7
BlackHawk0 <danielngoys@outlook.com>
2023-07-22 03:03:16 +0300

    ✨: User profile feature

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,58 +1,176 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useContext } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, XMarkIcon, Bars2Icon } from '@heroicons/react/24/outline'
 import { AppContext } from '../../context/context';
+import { Link } from 'react-router-dom';
+
+const navigation = [
+  { name: 'Feed', href: '/', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 const Header = () => {
-  const { isLoggedIn, setIsLoggedIn} = useContext(AppContext);
-  const isPremiumUser = true; 
-  console.log(isLoggedIn);
+  const { isLoggedIn, setIsLoggedIn } = useContext(AppContext);
 
   const handleLogout = () => {
-    // Clear any user-related data from local storage and reset isLoggedIn state
-    // localStorage.removeItem('postsViewedToday'); // Clear postsViewedToday data
-    setIsLoggedIn(false);
-    navigate('/login'); // Redirect to the login page after logout
+    setIsLoggedIn(false); // Call the logout function in the context to set isLoggedIn to false
   };
 
   return (
-    <nav className="bg-blue-500 p-4 text-white">
-      <ul className="flex justify-between items-center">
-        <li>
-          <Link to="/feeds">Feed</Link>
-        </li>
-        {isLoggedIn && (
-          <>
-            <li>
-              <Link to="/my-posts">My Posts</Link>
-            </li>
-            {isPremiumUser && (
-              <>
-                <li>
-                  <Link to="/following">Following</Link>
-                </li>
-                <li>
-                  <Link to="/premium">Premium</Link>
-                </li>
-                <li>
-                  <Link to="/block-posts">Block Posts</Link>
-                </li>
-              </>
-            )}
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <button onClick={handleLogout}>Logout</button>
-            </li>
-          </>
-        )}
-        {!isLoggedIn && (
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars2Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'px-3 py-2 rounded-md text-sm font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              {isLoggedIn ? ( // Render profile dropdown if user is logged in
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                  <button
+                    type="button"
+                    className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800"
+                  >
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                  <Menu as="div" className="ml-3 relative">
+                    <div>
+                      <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxOTk4MzV8MHwxfGFsbHwxfHx8fHx8fHwxNjExODUxNjMw&ixlib=rb-1.2.1&q=80&w=400"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                  'block px-4 py-2 text-sm'
+                                )}
+                              >
+                                Your Profile
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                  'block px-4 py-2 text-sm'
+                                )}
+                              >
+                                Settings
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                onClick={handleLogout}
+                                className={classNames(
+                                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                  'block px-4 py-2 text-sm'
+                                )}
+                              >
+                                Sign out
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              ) : (
+                // Render the "Login" link for non-logged-in users
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                  <Link to="/login" className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Login
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Mobile menu */}
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
 
